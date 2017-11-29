d3e561bdc6f9a211c4ba9a73a285af18bcd06d49
Nicolai Garcia <nicogarcia@DN800c3707.stanford.edu>
2017-11-29 00:24:10 -0800

    Milestone 1 UI Skeleton Completed

---
+++ Wall/Wall/Content View Controllers/SocialMediaImportViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SocialMediaImportViewController.swift
+//  Wall
+//
+//  Created by Nicolai Garcia on 11/28/17.
+//  Copyright © 2017 Nicolai Garcia. All rights reserved.
+//
+
+import UIKit
+
+class SocialMediaImportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
