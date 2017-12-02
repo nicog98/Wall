78a80e75637abecaa2c630eac78675a59ad8de14
Nicolai Garcia <nicogarcia@DN0a226604.SUNet>
2017-12-01 16:29:45 -0800

    Milestone 1 complete

---
+++ Wall/Wall/Content Collection View Cells/JournalWallCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  JournalWallCollectionViewCell.swift
+//  Wall
+//
+//  Created by Nicolai Garcia on 12/1/17.
+//  Copyright © 2017 Nicolai Garcia. All rights reserved.
+//
+
+import UIKit
+
+class JournalWallCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Wall/Wall/WallDocument.swift
@@ -0,0 +1,13 @@
+//
+//  WallDocument.swift
+//  Wall
+//
+//  Created by Nicolai Garcia on 12/1/17.
+//  Copyright © 2017 Nicolai Garcia. All rights reserved.
+//
+
+import UIKit
+
+class WallDocument: UIDocument {
+
+}
