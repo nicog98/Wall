fe51a666dfb4eaf6b04b913b691bcb59e030d82a
Nicolai Garcia <nicogarcia@DN800c3709.stanford.edu>
2017-12-10 21:25:26 -0800

    Implemented Slider to change content size.

---
+++ Wall/Wall/SliderViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SliderViewController.swift
+//  Wall
+//
+//  Created by Nicolai Garcia on 12/10/17.
+//  Copyright © 2017 Nicolai Garcia. All rights reserved.
+//
+
+import UIKit
+
+class SliderViewController: UIViewController {
+    
+    @IBAction func slider(_ sender: UISlider) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
